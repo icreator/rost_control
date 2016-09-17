7ee2331c879ef312426c73799c52032068f7c4b9
Dmitrii Ermolaev <icreator@mail.ru>
2016-09-17 23:22:35 +0300

    Add files via upload

--- web2py/hashes/controllers/api.py
+++ web2py/hashes/controllers/api.py
@@ -57,6 +57,30 @@ def get():
     #8 926 530 7393 Андрей
     return {'rows': rows, 'count': db(db.t_files.id > 0).count()}
 
+def get_uiks():
+    from_rec = request.args(0) or 0
+    limit_rec = request.args(1) or 100
+    try:
+        from_rec = int(from_rec)
+        limit_rec = int(limit_rec)
+    except:
+        return {'error':'error'}
+
+    if limit_rec and from_rec:
+        recs = db().select(db.t_uik.ALL, limitby=(from_rec, from_rec + limit_rec))
+    else:
+        recs = db(db.t_uik).select(limitby=(0, limit_rec))
+        
+    rows = {}
+    for row in recs:
+        row.pop('delete_record')
+        row.pop('update_record')
+        rows[row.id] = row
+    
+    #8 926 530 7393 Андрей
+    return {'rows': rows, 'count': db(db.t_uik.id > 0).count()}
+
+
 
 def set_txid1():
     from_rec = request.args(0) or 0
